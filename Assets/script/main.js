eb6546063b8d69a8b6aa9e834fdbc0b1b3a7d5a0
sughino <ludograsso08@gmail.com>
2025-12-11 16:04:13 +0100

    added numbers animation

--- Assets/script/main.js
+++ Assets/script/main.js
@@ -1,3 +1,68 @@
-//TODO inserire codice per animazione numeri
+//* number animation
+(function($) {
+
+  $.fn.digitAnimation = function(value, options = {}) {
+
+    const settings = $.extend({
+      duration: 800,
+      delayBetweenDigits: 200,
+      easing: "ease-out"
+    }, options);
+
+    function animateNumberContainers($digits, number) {
+        const digits = number.toString().split("");
+
+        $digits.each(function(index) {
+            const $digitContainer = $(this);
+            const targetDigit = digits[index] ?? 0;
+
+            const $numbers = $digitContainer.find(".number");
+            const digitHeight = $numbers.outerHeight();
+
+            let lastIndex = 0;
+            $numbers.each(function(i) {
+                if (parseInt($(this).text()) === parseInt(targetDigit)) lastIndex = i;
+            });
+
+            const translateY = -digitHeight * lastIndex;
+
+            const delay = settings.delayBetweenDigits * index;
+
+            setTimeout(() => {
+            $digitContainer.css({
+                transition: `transform ${settings.duration}ms ${settings.easing}`,
+                transform: `translateY(${translateY}px)`
+            });
+            }, delay);
+        });
+    }
+
+
+    function animateOnView($element, run) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            run();
+            observer.disconnect();
+          }
+        });
+      });
+      observer.observe($element[0]);
+    }
+
+    return this.each(function() {
+      const $group = $(this);
+      const $digits = $group.find(".number-container");
+
+      animateOnView($group, () => {
+        animateNumberContainers($digits, value);
+      });
+    });
+  };
+
+})(jQuery);
+
+$("#numbers-animation-1").digitAnimation(100);
+$("#numbers-animation-2").digitAnimation(500);
 
 //TODO inserire codice per inserire le immagini dinamicamente
